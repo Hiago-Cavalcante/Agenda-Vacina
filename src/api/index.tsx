bf173beb9235065d8c9444cd5724e2576bd11e82
Hiago Cavalcante <hiagocavalcante03@gmail.com>
2024-12-18 17:14:41 -0300

    add api get

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,1 +1,14 @@
+import axios from "axios";
 
+const url = "http://localhost:8080";
+
+export async function getAlergias() {
+  try {
+    const response = await axios.get(`${url}/alergias`);
+    console.log(response.data.data); // Use response.data para acessar os dados retornados.
+    return response.data.data; // Retorne os dados, se necessário.
+  } catch (error) {
+    console.error("Erro ao buscar alergias:", error);
+    throw error; // Lance o erro para tratamento adicional, se necessário.
+  }
+}
