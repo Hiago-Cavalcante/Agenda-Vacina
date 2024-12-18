59396910dcffad0207635797a7c015bd10dab607
Hiago Cavalcante <hiagocavalcante03@gmail.com>
2024-12-18 17:16:01 -0300

    table e btn novo na aba de agendas

--- src/components/pages/AgendaPage/index.tsx
+++ src/components/pages/AgendaPage/index.tsx
@@ -12,11 +12,18 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const AgendaPage = function () {
   const { handleButtonHomePage } = useHandleEvents();
-  const [dataInit, setDataInit] = useState<Dayjs | null>(dayjs(null));
-  const [dataEnd, setDataEnd] = useState<Dayjs | null>(dayjs(null));
+  const [dataInit, setDataInit] = useState<Dayjs | null>(dayjs());
+  const [dataEnd, setDataEnd] = useState<Dayjs | null>(dayjs());
 
   return (
     <div className="agendas_page">
@@ -30,6 +37,16 @@ const AgendaPage = function () {
           <ArrowBackIosIcon style={{ fontSize: "medium" }} /> VOLTAR
         </Button>
         <h1>Agendas</h1>
+        <div className="btn_add_agenda">
+          <Button
+            variant="contained"
+            color="primary"
+            // onClick={handleOpenDialog}
+            style={{ marginBottom: 16 }}
+          >
+            Adicionar Nova Agenda
+          </Button>
+        </div>
       </div>
       <div className="agendas_content">
         <FormControl fullWidth>
@@ -39,9 +56,10 @@ const AgendaPage = function () {
             id="demo-simple-select"
             label="Age"
           >
-            <MenuItem>Ten</MenuItem>
-            <MenuItem>Twenty</MenuItem>
-            <MenuItem>Thirty</MenuItem>
+            <MenuItem>Todas</MenuItem>
+            <MenuItem>Agendada</MenuItem>
+            <MenuItem>Concluida</MenuItem>
+            <MenuItem>Cancelada</MenuItem>
           </Select>
         </FormControl>
         <FormControl fullWidth>
@@ -51,9 +69,8 @@ const AgendaPage = function () {
             id="demo-simple-select"
             label="Age"
           >
-            <MenuItem>Ten</MenuItem>
-            <MenuItem>Twenty</MenuItem>
-            <MenuItem>Thirty</MenuItem>
+            <MenuItem>Todos</MenuItem>
+            <MenuItem>Usuario </MenuItem>
           </Select>
         </FormControl>
       </div>
@@ -74,6 +91,25 @@ const AgendaPage = function () {
             />
           </DemoContainer>
         </LocalizationProvider>
+      </div>
+      <div className="agenda_table">
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Data</TableCell>
+                <TableCell>Observações</TableCell>
+                <TableCell>Vacina</TableCell>
+                <TableCell>Dose</TableCell>
+                <TableCell>Usuário</TableCell>
+                <TableCell>Situação</TableCell>
+                <TableCell>Data situação</TableCell>
+                <TableCell>Ações</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody></TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
