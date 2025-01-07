18734ca414de85ed5b7ffaafa4426ea1cf1b0869
Hiago Cavalcante <hiagocavalcante03@gmail.com>
2025-01-07 15:15:22 -0300

    vacinas update

--- src/components/pages/VacinasPage/index.tsx
+++ src/components/pages/VacinasPage/index.tsx
@@ -1,9 +1,17 @@
 import useHandleEvents from "../../BtnNavigate";
 import "./vacinas.css";
-import { Button } from "@mui/material";
+import {
+  Button,
+  paperClasses,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-const VacPage = function () {
+const VacPage: React.FC = function () {
   const { handleButtonHomePage } = useHandleEvents();
   return (
     <div className="vac_page">
@@ -17,6 +25,31 @@ const VacPage = function () {
           <ArrowBackIosIcon style={{ fontSize: "medium" }} /> VOLTAR
         </Button>
         <h1>Vacinas</h1>
+        <div className="vac_btn_add">
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ marginBottom: 16, height: "40px" }}
+          >
+            Adicionar Vacina
+          </Button>
+        </div>
+      </div>
+      <div className="vac_table">
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: "bold" }}>Titulo</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Descrição</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Doses</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Periodicidade</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Intervalo</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Ações</TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
